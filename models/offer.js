0d9dcd62659df71f1bb28ad579c876ed77da6fe4
Your Name <you@example.com>
2019-04-29 09:34:34 +0200

    Actualización 3- Introduccion datos en tabla

--- models/offer.js
+++ models/offer.js
@@ -2,14 +2,27 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const OfferSchema = new Schema({
-    title: String,
-    description: String,
-    requirements: String,
-    salary: Number,
+    post: {
+      type: String,
+      required: [true,'puesto es requerido']
+    },
+    description: {
+      type: String,
+      required: [true,'la descripción es requerida']
+    },
+    requirements: {
+      type: [String],
+      default: ''
+    },
+    salary: {
+      type: Number,
+      default: 0
+    },
     endOffer: {
         type: Date,
         default: Date.now
-      }
-})
+      },
+});
 
 module.exports = mongoose.model('offers', OfferSchema);
+
--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,21 @@ var router = express.Router();
 
 const Offer = require('../models/offer');
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  const offers = await Offer.find();
+  res.render('index', {
+    offers
+  });
 });
 
-router.post('/add', (req, res, next) => {
-  console.log(new Offer(req.body));
-  res.send('recibido');
+router.post('/add', async (req, res, next) => {
+  const offer = new Offer(req.body);
+  await offer.save();
+  res.redirect('/');
 });
-
+router.get('/delete/:id', async (req, res) => {
+  const { id } = req.params;
+  await Offer.remove({id: id});
+  res.redirect('/');
+});
 module.exports = router;
