dda5ad11dc486d998f0227f3d6ecd2403ff2c7cf
Your Name <you@example.com>
2019-04-29 20:39:25 +0200

    4 actualizacion- Boton Borrar

--- routes/index.js
+++ routes/index.js
@@ -15,9 +15,33 @@ router.post('/add', async (req, res, next) => {
   await offer.save();
   res.redirect('/');
 });
-router.get('/delete/:id', async (req, res) => {
+
+router.get('/delete/:id', (req, res) => {
   const { id } = req.params;
-  await Offer.remove({id: id});
-  res.redirect('/');
+
+  Offer.deleteOne({_id:id})
+  .then((user)=>{res.redirect('/')})
+  .catch((err)=>{res.redirect('/'), err})
 });
+module.exports = router;
+
+/*router.get('/edit/:id', (req, res, next) => {
+  Offer.findOne({_id: req.query.offer_id})
+  .then((offer) => {
+    res.render("edit-offer", {offer});
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+router.post('/edit/:id', (req, res, next) => {
+  const { puesto, descripcion, requisitos, salario } = req.body;
+  Offer.update({_id: req.query.offer_id}, { $set: {puesto, descripcion, requisitos, salario }})
+  .then((offer) => {
+    res.redirect('/');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});*/
 module.exports = router;
